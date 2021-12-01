e498615a2e47fd7114e81adae106e7ea24773e01
Alimur Razi Rana <alimurrazi570@gmail.com>
2021-12-01 09:03:58 +0600

    feat: mongodb add

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config();
+const connectDB = require('./config/db');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+connectDB();
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
